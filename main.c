0fdf77dc379dfa2f50a53f9b788ca227c011ce7d
kibibyte137 <01197807@pw.edu.pl>
2025-03-19 22:27:25 +0100

    Dodanie obslugi bledow wczytywania argumentow.

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int czy_istnieje(const char *plik) {
+    FILE *plik = fopen(plik, "r");
+    if(plik){
+        fclose(plik);
+        return 1; /*Plik istnieje*/
+    }
+    return 0; /*Plik nie istnieje*/
+}
+
 /*Funkcja main programu*/
 int main(int argc, char *argv[]){
 	int opt; /*Zmienna przechowujaca opcje getopt*/
@@ -42,11 +51,39 @@ int main(int argc, char *argv[]){
 		}
 	}
 	
+	
+	/*OBSLUGA BLEDOW*/
+	
+	if !czy_istnieje(plik){
+		printf("Nie znaleziono pliku: %s.", plik);
+		return 1;
+	}
+	
+	if(format_wyjsciowy != "txt" && format_wyjsciowy != "bin"){
+		printf("Podano nieprawidlowy format pliku wyjsciowego: %s.", format_wyjsciowy);
+		return 2;
+	}
+	
+	if(margines < 0 || margines > 100){
+		printf("Podano niepoprawny margines procentowy: %s.", margines);
+		return 3;
+	}
+	
+	/*OBSLUGA BLEDOW PO ODCZYTANIU GRAFU*/
+	
+	/*Niepoprawny format pliku: Nie udalo sie odczytac pliku: plik. Kod powrotu programu 5*/
+	
+	/* V - liczba wierzchołków w grafie
+	if(czesci < 0 || czesci > V){
+		printf("Nie mozna podzielic grafu na podana liczbe czesci: %s.", czesci);
+		return 3;
+	}
+	*/
+	
 	/*DEBUG*/
 	printf("Podany plik to: %s.\n", plik ? plik : "BRAK");
 	printf("Przyjety format wyjsciowy to: %s.\n", format_wyjsciowy);
 	printf("Przyjeta liczba czesci to: %d.\n", czesci);
 	printf("Przyjety margines procentowy to: %d.\n", margines); 
-	
 	return 0;
 }
